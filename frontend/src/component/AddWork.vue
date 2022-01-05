8afd3ed3e8adb82b22dc12c8764ecee9134e1656
stingion <stingion@gmail.com>
2022-01-05 16:44:27 +0200

    #16 Intro Work add functionality

--- frontend/src/component/AddWork.vue
+++ frontend/src/component/AddWork.vue
@@ -5,5 +5,116 @@
   -->
 
 <template>
+  <div class="submit-form">
+    <div v-if="!submitted">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input
+            type="text"
+            class="form-control"
+            id="title"
+            required
+            v-model="work.title"
+            name="title"
+        />
+      </div>
 
+      <div class="form-group">
+        <label for="description">Description</label>
+        <input
+            class="form-control"
+            id="description"
+            required
+            v-model="work.description"
+            name="description"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="price">Price</label>
+        <input
+            class="form-control"
+            id="price"
+            required
+            v-model="work.price"
+            name="price"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="endDate">End Date</label>
+        <input
+            class="form-control"
+            id="endDate"
+            required
+            v-model="work.endDate"
+            name="endDate"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="payDate">Pay Date</label>
+        <input
+            class="form-control"
+            id="payDate"
+            required
+            v-model="work.payDate"
+            name="payDate"
+        />
+      </div>
+
+      <button @click="saveWork" class="btn btn-success">Submit</button>
+    </div>
+
+    <div v-else>
+      <h4>You submitted successfully!</h4>
+      <button class="btn btn-success" @click="newWork">Add</button>
+    </div>
+  </div>
 </template>
+
+<script>
+import workDataService from "@/service/WorkDataService"
+
+export default {
+  name: 'add-work',
+
+  data: () => ({
+    work: {
+      id: null,
+      title: '',
+      price: 0,
+      endDate: null,
+      payDate: null
+    },
+    submitted: false
+  }),
+  methods: {
+    saveWork() {
+      const data = {
+        title: this.work.title,
+        description: this.work.description,
+        price: this.work.price,
+        endDate: this.work.endDate,
+        payDate: this.work.payDate,
+      };
+
+      workDataService.create(data)
+          .then(response => {
+            this.work.id = response.data.id;
+            console.log(response.data);
+            this.submitted = true;
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    newWork() {
+      this.submitted = false;
+      this.work = {};
+    }
+  }
+}
+
+</script>
--- frontend/src/component/WorkList.vue
+++ frontend/src/component/WorkList.vue
@@ -8,25 +8,26 @@
   <div>
     <ul class="list-group">
       <li class="list-group-item row d-flex border-3">
-        <div class="list-item col-sm border-end border-1">
+        <div class="list-item col-sm border-end border-3">
           <strong>Id</strong>
         </div>
-        <div class="list-item col-sm border-end border-1">
+        <div class="list-item col-sm border-end border-3">
           <strong>Title</strong>
         </div>
-        <div class="list-item col-sm border-end border-1">
+        <div class="list-item col-sm border-end border-3">
           <strong>Description</strong>
         </div>
-        <div class="list-item col-sm border-end border-1">
+        <div class="list-item col-sm border-end border-3">
           <strong>Price</strong>
         </div>
-        <div class="list-item col-sm border-end border-1">
+        <div class="list-item col-sm border-end border-3">
           <strong>End date</strong>
         </div>
         <div class="list-item col-sm">
           <strong>Pay date</strong>
         </div>
       </li>
+
       <WorkItem v-for="(work, idx) in works"
                 :work="work"
                 :idx="idx"
