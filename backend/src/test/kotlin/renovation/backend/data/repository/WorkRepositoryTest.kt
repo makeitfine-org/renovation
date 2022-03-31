fe139e56de6d696e2bcec2aa06a56d622f9abae3
stingion <stingion@gmail.com>
2022-03-31 23:10:19 +0300

    #14 Fix h2 test config

--- backend/src/test/kotlin/renovation/backend/data/repository/WorkRepositoryTest.kt
+++ backend/src/test/kotlin/renovation/backend/data/repository/WorkRepositoryTest.kt
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -23,6 +24,7 @@ import java.time.LocalDate
 @IntegrationTest
 @ExtendWith(SpringExtension::class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 internal class WorkRepositoryTest(
     @Autowired private val entityManager: TestEntityManager,
     @Autowired private val workRepository: WorkRepository
