e1909afc8e5d7105e55a18a39b66232b2de792ef
stingion <stingion@gmail.com>
2022-04-02 15:16:28 +0300

    #31 exclude mockito lib

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,7 +20,6 @@ dependencyManagement {
 dependencies {
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.postgresql:postgresql:${properties["postgresqlVersion"]}")
-
     implementation("org.springframework.boot:spring-boot-starter-web") {
         exclude(group = "com.fasterxml.jackson", module = "jackson-bom")
         exclude(group = "com.fasterxml.jackson.core", module = "jackson-databind")
@@ -28,23 +27,22 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator") {
         exclude(group = "com.fasterxml.jackson.core", module = "jackson-databind")
     }
-
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.liquibase:liquibase-core:${properties["liquibaseVersion"]}")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") {
+        exclude("org.hibernate:hibernate-core")
+    }
+    implementation("org.hibernate:hibernate-core:${properties["hibernateVersion"]}")
+
+    runtimeOnly("com.h2database:${properties["h2Version"]}")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "junit")
         exclude(group = "org.junit.jupiter")
         exclude(group = "org.junit.vintage")
+        exclude(group = "org.mockito")
     }
-
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa") {
-        exclude("org.hibernate:hibernate-core")
-    }
-    implementation("org.hibernate:hibernate-core:${properties["hibernateVersion"]}")
-    runtimeOnly("com.h2database:${properties["h2Version"]}")
-
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("io.mockk:mockk:${properties["mockkVersion"]}")
--- backend/src/main/kotlin/renovation/backend/data/repository/WorkRepository.kt
+++ backend/src/main/kotlin/renovation/backend/data/repository/WorkRepository.kt
@@ -6,17 +6,11 @@
 
 package renovation.backend.data.repository
 
-import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import renovation.backend.data.entity.WorkEntity
 
 @Repository
 interface WorkRepository : JpaRepository<WorkEntity, Long> {
-    override fun findAll(): List<WorkEntity>
-
-    override fun findAll(sort: Sort): List<WorkEntity>
-
     fun findByTitleLike(titleLikePattern: String): List<WorkEntity>
 }
