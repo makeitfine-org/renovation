0fb99994f707d50ee69f16c33e48789769b21d98
stingion <stingion@gmail.com>
2022-09-15 17:35:00 +0300

    #78 Refactoring GrantTypeAccessToken impls

--- common/src/main/kotlin/renovation/common/iam/GrantTypeAccessToken.kt
+++ common/src/main/kotlin/renovation/common/iam/GrantTypeAccessToken.kt
@@ -8,13 +8,38 @@ package renovation.common.iam
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestTemplate
 
 interface GrantTypeAccessToken : Token {
     val grantType: String
+    val tokenEndpoint: String
     override val token: String
 
     fun bearerAuthorizationHeader() =
         Header("Authorization", "Bearer $token")
+
+    fun obtainToken(vararg keyValues: Pair<String, String>) = LinkedMultiValueMap<String, String>().let {
+        keyValues.forEach { kv ->
+            it[kv.first] = kv.second
+        }
+
+        it["grant_type"] = grantType
+
+        RestTemplate().postForObject(
+            tokenEndpoint,
+            HttpEntity(
+                it,
+                HttpHeaders().also {
+                    it.contentType = MediaType.APPLICATION_FORM_URLENCODED
+                }
+            ),
+            AccessToken::class.java
+        )!!.accessToken
+    }
 
     data class Header(
         val headerName: String,
--- common/src/main/kotlin/renovation/common/iam/impl/PasswordGrantAccessToken.kt
+++ common/src/main/kotlin/renovation/common/iam/impl/PasswordGrantAccessToken.kt
@@ -6,39 +6,21 @@
 
 package renovation.common.iam.impl
 
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
-import org.springframework.util.LinkedMultiValueMap
-import org.springframework.web.client.RestTemplate
 import renovation.common.iam.GrantTypeAccessToken
-import renovation.common.iam.GrantTypeAccessToken.AccessToken
 
 data class PasswordGrantAccessToken(
     val clientId: String,
     val clientSecret: String,
     val username: String,
     val password: String,
-    val tokenEndpoint: String,
+    override val tokenEndpoint: String,
 ) : GrantTypeAccessToken {
     override val grantType = "password"
 
-    override val token: String = LinkedMultiValueMap<String, String>().let {
-        it["grant_type"] = grantType
-        it["client_id"] = clientId
-        it["client_secret"] = clientSecret
-        it["username"] = username
-        it["password"] = password
-
-        RestTemplate().postForObject(
-            tokenEndpoint,
-            HttpEntity(
-                it,
-                HttpHeaders().also {
-                    it.contentType = MediaType.APPLICATION_FORM_URLENCODED
-                }
-            ),
-            AccessToken::class.java
-        )!!.accessToken
-    }
+    override val token: String = super.obtainToken(
+        "client_id" to clientId,
+        "client_secret" to clientSecret,
+        "username" to username,
+        "password" to password,
+    )
 }
