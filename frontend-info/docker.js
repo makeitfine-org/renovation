cdcde5ea75a42957c2c58a4a61f44a9137335075
stingion <stingion@gmail.com>
2022-09-18 17:15:24 +0300

    #78 Fix docker.js frontend-info module

--- frontend-info/docker.js
+++ frontend-info/docker.js
@@ -26,11 +26,14 @@ router.get('/check', function (req, res) {
     return res.send(message)
 })
 
+let accessToken = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ6eFZ4RGx2UFE2dGFhYVdHTnRCMDE4bXk5VnhqbDBCRTlIZXY5RzBRWjJvIn0.eyJleHAiOjE2NjM1MTE4NDksImlhdCI6MTY2MzUxMDM0OSwianRpIjoiOTk0YmU2MGQtYzk1MS00MWIyLTlhZTUtZTlhN2FhYWVlMTRhIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDoxODA4MC9yZWFsbXMvcmVub3ZhdGlvbi1yZWFsbSIsInN1YiI6ImQyMDU4ODIxLTY2YWUtNDhlZC1iNWI0LWFkOGMzNTU4OWYzNyIsInR5cCI6IkJlYXJlciIsImF6cCI6InJlbm92YXRpb24tY2xpZW50Iiwic2Vzc2lvbl9zdGF0ZSI6ImEyYTc4Y2RmLWQ0MzQtNDdkZi05NmQ4LTc1MTZhZTdhMWZlOSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJkZWZhdWx0LXJvbGVzLXJlbm92YXRpb24iLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7InJlbm92YXRpb24tY2xpZW50Ijp7InJvbGVzIjpbIndvcmsiLCJhZG1pbiIsIndvcmtlciJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsInNpZCI6ImEyYTc4Y2RmLWQ0MzQtNDdkZi05NmQ4LTc1MTZhZTdhMWZlOSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhbGwtdGVzdCJ9.NGFWYcyYxXPhtH1GoRdCg_6NRDNTGpbwYm9yODtkirFCrZAosMso70tfupkUIyEBwGgPwmhaVi3e3LpnyXoSoPeNOYPL8Ealbvol2BbI7kpUelJzme9DtSe1mhUmX2fqhWxZmRas44HNlPMeRO15uGF-PlRg4-qAu8dWk346T5ta0M0OxtLRT940pmXWyoITr1J1AGanL4hEko81RYF7_OUiNLPE-c1sEZ-StBNRmJ-kul9cS2itUqxzpMsvigc7Meeikp4htQevFO1LxSSm7EdgHefJUJGr8ym3pqrz8fMw9g9P-7ON74gES-lRmxqsP21M3v1Wny4U6YEXVlUHRw"
+
 app.get('/worker', function (req, res) {
     let url = `${process.env.VUE_APP_BACKEND_API_URL}/worker`
 
     axios({
         method: 'get',
+        headers: {"Authorization": `Bearer ${accessToken}`},
         url
     })
         .then(function (response) {
@@ -59,7 +62,9 @@ app.get('/graphql', function (req, res) {
             `
     }
 
-    axios.post(url, data)
+    axios.post(url, data, {
+        headers: {"Authorization": `Bearer ${accessToken}`},
+    })
         .then(function (response) {
             res.setHeader("Content-Type", "application/json")
             res.send(JSON.stringify(response.data))
