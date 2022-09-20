bd2e96a4d1dfbd19775830f45e27f32cabfb7303
stingion <stingion@gmail.com>
2022-09-20 14:53:25 +0300

    #78 actuator tests fix

--- backend-api-test/src/test/kotlin/renovation/backend/api/test/serverapi/ActuatorCheckTest.kt
+++ backend-api-test/src/test/kotlin/renovation/backend/api/test/serverapi/ActuatorCheckTest.kt
@@ -9,35 +9,42 @@ package renovation.backend.api.test.serverapi
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
+import kotlin.test.Test
 import org.apache.http.HttpStatus
 import org.junit.jupiter.api.Tag
 import renovation.backend.api.test.ServerRoute
-import kotlin.test.Test
+import renovation.backend.api.test.helper.SecurityHelper
 
 @Tag("healthCheck")
 internal class ActuatorCheckTest {
 
     @Test
     fun `actuator health`() {
-        Given {
-            header("Content-type", "application/json")
-        }
-        When {
-            get(ServerRoute.ACTUATOR_HEALTH.route)
-        }.Then {
-            statusCode(HttpStatus.SC_OK)
-        }
+        given()
+            .When {
+                get(ServerRoute.ACTUATOR_HEALTH.route)
+            }
+            .Then {
+                statusCode(HttpStatus.SC_OK)
+            }
     }
 
     @Test
     fun `actuator info`() {
-        Given {
-            header("Content-type", "application/json")
-        }
-        When {
-            get(ServerRoute.ACTUATOR_INFO.route)
-        }.Then {
-            statusCode(HttpStatus.SC_OK)
+        given()
+            .When {
+                get(ServerRoute.ACTUATOR_INFO.route)
+            }
+            .Then {
+                statusCode(HttpStatus.SC_OK)
+            }
+    }
+
+    private fun given() = Given {
+        header("Content-type", "application/json")
+
+        SecurityHelper.obtainPasswordGrantAccessToken().bearerAuthorizationHeader().let {
+            header(it.headerName, it.headerValue)
         }
     }
 }
