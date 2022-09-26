1960c46b3c9a91ab4bd27c890de6c2ca9ac5b909
stingion <stingion@gmail.com>
2022-09-26 12:35:11 +0300

    #8 Add for Worker entity

--- backend/src/main/kotlin/renovation/backend/data/domain/Worker.kt
+++ backend/src/main/kotlin/renovation/backend/data/domain/Worker.kt
@@ -6,9 +6,15 @@
 
 package renovation.backend.data.domain
 
+import java.io.Serializable
+
 data class Worker(
     val id: String? = null,
     val name: String? = null,
     val surname: String? = null,
     val age: Int? = null,
-)
+) : Serializable {
+    companion object {
+        const val serialVersionUID = 1L
+    }
+}
