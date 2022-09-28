e6d9f13f5c6c70c434f073724f6f20c2825f88d5
stingion <stingion@gmail.com>
2022-09-28 19:22:46 +0300

    #64 Fix refres bug

--- backend/src/main/kotlin/renovation/backend/web/controller/RouteController.kt
+++ backend/src/main/kotlin/renovation/backend/web/controller/RouteController.kt
@@ -15,7 +15,7 @@ const val FORWARD = "forward:/"
 class RouteController {
 
     // todo: simplify it or move to separate config
-    @RequestMapping("/home", "/fe", "/fe/*")
+    @RequestMapping("/home", "/fe", "/fe/**")
     // @RequestMapping("/{path:[^\\.]*}")
     fun redirect() = FORWARD
 }
