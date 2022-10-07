62444f6bb15b6ce89b9b0565879eddbdb32bb9a6
stingion <stingion@gmail.com>
2022-10-07 18:33:18 +0300

    #78 Gateway test in process

--- gateway/src/test/kotlin/renovation/gateway/config/ExposeApiConfigTest.kt
+++ gateway/src/test/kotlin/renovation/gateway/config/ExposeApiConfigTest.kt
@@ -6,19 +6,63 @@
 
 package renovation.gateway.config
 
+import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import kotlin.test.Test
 import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.test.context.ActiveProfiles
+import renovation.common.iam.GrantTypeAccessToken
+import renovation.common.iam.impl.PasswordGrantAccessToken
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 internal class ExposeApiConfigTest(
-    @LocalServerPort val port: Int
+    @LocalServerPort private val port: Int,
+    @Autowired private val creds: AllTestUserCredentials,
 ) {
     val portHost = "http://localhost:$port"
+
+    @TestConfiguration
+    class ControllerTestConfig {
+
+        @Bean
+        fun passwordGrantAccessToken(
+            @Value("\${spring.security.oauth2.client.registration.oauth-client.client-id}")
+            clientId: String,
+            @Value("\${spring.security.oauth2.client.registration.oauth-client.client-secret}")
+            clientSecret: String,
+            @Value("\${spring.security.oauth2.client.provider.oauth-client.token-uri}")
+            tokenUri: String,
+            creds: AllTestUserCredentials
+        ) = PasswordGrantAccessToken(
+            clientId,
+            clientSecret,
+            creds.username,
+            creds.password,
+            tokenUri
+        )
+    }
+
+    @Autowired
+    private lateinit var token: GrantTypeAccessToken
+
+    fun given() = Given {
+        token.bearerAuthorizationHeader().let {
+            port(port)
+                .and()
+                .header(it.headerName, it.headerValue)
+        }
+    }
 
     @Test
     fun `Assert about controller`() {
@@ -34,4 +78,24 @@ internal class ExposeApiConfigTest(
             )
         }
     }
+
+    @Test
+    fun `Assert user controller`() {
+        given()
+            .When {
+                get("$portHost/user")
+            }.Then {
+                statusCode(HttpStatus.SC_OK)
+                body(
+                    CoreMatchers.equalTo("user")
+                )
+            }
+    }
+}
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.security.credentials")
+class AllTestUserCredentials {
+    lateinit var username: String
+    lateinit var password: String
 }
