08f1f3f84b044b8fff07c1a199fcf1b7d22ca21e
stingion <stingion@gmail.com>
2023-06-09 23:34:21 +0300

    #67 bootstrap offline

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,7 @@ import {createApp} from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
 
 createApp(App)
     .use(router)
