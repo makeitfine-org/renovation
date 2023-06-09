73dd388c9d1db29942a961e5486ca477811df1c8
stingion <stingion@gmail.com>
2023-06-09 19:12:39 +0300

    #67 NotEmpty validation changed to NotBlank

--- info/src/main/kotlin/renovation/info/data/model/DetailsEntity.kt
+++ info/src/main/kotlin/renovation/info/data/model/DetailsEntity.kt
@@ -14,7 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document
 import renovation.info.generated.dgs.types.Gender
 import javax.validation.constraints.Max
 import javax.validation.constraints.Min
-import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
@@ -24,11 +24,11 @@ data class DetailsEntity(
     @Id
     var id: ObjectId? = ObjectId.get(),
 
-    @field:NotEmpty
+    @field:NotBlank
     @field:Size(min = 2, max = 25, message = "Name size should have not less 2 and not more 25 symbols")
     var name: String? = null,
 
-    @field:NotEmpty
+    @field:NotBlank
     @field:Size(min = 2, max = 25, message = "Surname size should have not less 2 and not more 25 symbols")
     var surname: String? = null,
 
