9885a65e12e7f98f552d3a742fe991a6e96d9447
koresmosto <koresmosto@gmail.com>
2023-11-02 23:01:44 +0200

    #1 Improve nav menu

--- ng-part/src/app/component/navigation/navigation.component.ts
+++ ng-part/src/app/component/navigation/navigation.component.ts
@@ -1,9 +1,19 @@
 import {Component} from "@angular/core"
 
+export type Img = {
+  src: string,
+  alt: string,
+}
+
 @Component({
   selector: "app-navigation",
   templateUrl: "./navigation.component.html",
   styleUrls: [ "./navigation.component.scss" ]
 })
 export class NavigationComponent {
+
+  menuIcon: Img = {
+    src: "assets/images/menuIcon.svg",
+    alt: "Menu icon"
+  }
 }
