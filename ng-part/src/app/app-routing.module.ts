0fc5b27766de0508df665da67daf4b81135a9fb3
koresmosto <koresmosto@gmail.com>
2023-11-04 19:49:10 +0200

    #1 Intro todo and about in menu

--- ng-part/src/app/app-routing.module.ts
+++ ng-part/src/app/app-routing.module.ts
@@ -8,20 +8,24 @@ import {NgModule} from "@angular/core"
 import {RouterModule, Routes} from "@angular/router"
 import {TodoComponent} from "src/app/component/todo/todo.component"
 import {AboutPageComponent} from "src/app/component/about-page/about-page.component"
+import {AppComponent} from "./app.component"
 
 const routes: Routes = [
-  {path: "", component: TodoComponent},
+  //deprecated: to be rewritten and removed (for learning purposes)
+  {path: "about", component: AboutPageComponent},
   {path: "todo", component: TodoComponent},
+
+  {path: "", component: TodoComponent},
   {path: "about", component: AboutPageComponent},
 
   //todo: temp:
-  {path: "dashboard", component: AboutPageComponent},
-  {path: "ui", component: AboutPageComponent},
-  {path: "tables", component: AboutPageComponent},
-  {path: "forms", component: AboutPageComponent},
-  {path: "cards", component: AboutPageComponent},
-  {path: "modal", component: AboutPageComponent},
-  {path: "blank", component: AboutPageComponent},
+  {path: "dashboard", component: AppComponent},
+  {path: "ui", component: AppComponent},
+  {path: "tables", component: AppComponent},
+  {path: "forms", component: AppComponent},
+  {path: "cards", component: AppComponent},
+  {path: "modal", component: AppComponent},
+  {path: "blank", component: AppComponent},
 ]
 
 @NgModule({
