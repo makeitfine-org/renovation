2f9dbd063a1dc34a3d9e71e4aa51a0fab23168f1
koresmosto <koresmosto@gmail.com>
2023-11-05 13:13:56 +0200

    #1 White border in active menu item

--- ng-part/src/app/component/sidebar/sidebar.component.ts
+++ ng-part/src/app/component/sidebar/sidebar.component.ts
@@ -9,7 +9,7 @@ import {UiService} from "../../data/service/ui.service"
 export class SidebarComponent {
   @Input() title: string
 
-  readonly activeClass = "bg-gray-600 bg-opacity-25 text-white border-gray-100"
+  readonly activeClass = "border-gray-300 bg-gray-600 bg-opacity-25 text-white border-gray-100"
 
   readonly inactiveClass
     = "border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100"
