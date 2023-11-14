7760f23c87eb9958d474e9957cd4a55c212b1187
koresmosto <koresmosto@gmail.com>
2023-11-14 16:49:19 +0200

    #1 Intro error.service.spec.ts

--- ng-part/src/app/data/service/error.service.spec.ts
+++ ng-part/src/app/data/service/error.service.spec.ts
@@ -13,4 +13,20 @@ describe("ErrorService", () => {
   it("should be created", () => {
     expect(service).toBeTruthy()
   })
+
+  it("handle method", () => {
+    service.error$.subscribe((message: string) => {
+      expect(message).toEqual("some error message")
+    })
+
+    service.handle("some error message")
+  })
+
+  it("clear method", () => {
+    service.error$.subscribe((message: string) => {
+      expect(message).toEqual("")
+    })
+
+    service.clear()
+  })
 })
