742c5a19c65c8303a95cd529afc2c131f4a7848b
koresmosto <koresmosto@gmail.com>
2023-11-14 19:04:45 +0200

    #1 Intro todo-crud.service.spec.ts mock/spy

---
+++ ng-part/src/app/data/service/todo-crud.service.spec.ts
@@ -0,0 +1,73 @@
+/*
+ * Created under not commercial project "Renovation"
+ *
+ * Copyright 2021-2023
+ */
+
+import {HttpClient, HttpErrorResponse} from "@angular/common/http"
+import {TodoCrudService} from "./todo-crud.service"
+import {DatePipe} from "@angular/common"
+import {defer} from "rxjs"
+import {Todo} from "../model/todo.model"
+
+describe("TodoCrudService mock/spy", () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>
+  let todoCrudService: TodoCrudService
+  const datePipe = new DatePipe("en")
+
+  const TODOS: Todo[] = [
+    {id: 1, title: "I was born", completed: true},
+    {id: 2, title: "born Was", completed: true},
+    {id: 3, title: "quest it one", completed: false},
+    {id: 4, title: "born wAs", completed: true},
+    {id: 1, title: "Quest it one", completed: true},
+    {id: 1, title: "Quest it one again", completed: true},
+    {id: 1, title: "born was", completed: true}
+  ]
+
+  beforeEach(() => {
+    // TODO: spy on other methods too
+    httpClientSpy = jasmine.createSpyObj("HttpClient", [ "get" ])
+    todoCrudService = new TodoCrudService(httpClientSpy, datePipe)
+  })
+
+  it("get todos", (done: DoneFn) => {
+    httpClientSpy.get.and.returnValue(asyncData([ {id: 1, title: "t", completed: true} ]))
+
+    todoCrudService.getTodos().subscribe({
+      next: (todos) => {
+        expect(todos).withContext("expected todos").toEqual([ {id: 1, title: "t", completed: true} ])
+        done()
+      },
+      error: done.fail,
+    })
+
+    expect(httpClientSpy.get.calls.count()).withContext("one call").toBe(1)
+  })
+
+  it("should return an error when the server returns a 404", (done: DoneFn) => {
+    const errorResponse = new HttpErrorResponse({
+      error: "test 404 error",
+      status: 404,
+      statusText: "Not Found",
+    })
+
+    httpClientSpy.get.and.returnValue(asyncError(errorResponse))
+
+    todoCrudService.getTodos().subscribe({
+      next: (heroes) => done.fail("expected an error, not todos"),
+      error: (error) => {
+        expect(error.message).toContain("Http failure response for (unknown url): 404 Not Found")
+        done()
+      },
+    })
+  })
+
+  function asyncData<T>(data: T) {
+    return defer(() => Promise.resolve(data))
+  }
+
+  function asyncError<T>(errorObject: any) {
+    return defer(() => Promise.reject(errorObject))
+  }
+})
