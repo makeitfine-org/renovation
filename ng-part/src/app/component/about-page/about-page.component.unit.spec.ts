b31353de1db47dcf34ecc78d851b10517b3d6422
koresmosto <koresmosto@gmail.com>
2023-11-20 14:32:58 +0200

    #1 Intro about-page.component.unit.spec.ts

---
+++ ng-part/src/app/component/about-page/about-page.component.unit.spec.ts
@@ -0,0 +1,69 @@
+/*
+ * Created under not commercial project "Renovation"
+ *
+ * Copyright 2021-2023
+ */
+
+import {TestBed} from "@angular/core/testing"
+import {AboutPageComponent} from "./about-page.component"
+import {HttpClientTestingModule} from "@angular/common/http/testing"
+import {InfoService} from "../../data/service/info.service"
+import {of} from "rxjs"
+
+describe("AboutPageComponent ts (unit)", () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        // TitleCasePipe
+      ],
+      declarations: [
+        AboutPageComponent,
+        // MockPipe
+      ]
+    })//.compileComponents()
+  })
+
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AboutPageComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AboutPageComponent)
+    fixture.detectChanges()
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector("h1.font-bold.mb-2.text-center.text-lg")?.textContent).toContain("Extra Information")
+    expect(compiled.querySelector("div > p.mb-2 > span")?.textContent)
+      .toContain("Details of module used for backend requests:")
+
+    expect(Array.from(compiled.querySelectorAll("div > p.ml-2 > span")).map(e => e.textContent))
+      .toEqual([
+        " about ",
+        " description ",
+        " module info: "
+      ])
+  })
+
+  it(`should return about and module`, () => {
+    const fixture = TestBed.createComponent(AboutPageComponent)
+    const app = fixture.componentInstance
+    let service = fixture.debugElement.injector.get(InfoService)
+
+    spyOn(service, "getAbout").and.callFake(() => {
+      return of({name: "name about", description: "description about"})
+    })
+    spyOn(service, "getModule").and.callFake(() => {
+      return of("some module description")
+    })
+
+    app.ngOnInit()
+    expect(app.about).toEqual({name: "name about", description: "description about"})
+    expect(app.about).not.toEqual({name: "(other) name about", description: "(other) description about"})
+
+    expect(app.module).toEqual("some module description")
+    expect(app.module).not.toEqual("(other) some module description")
+  })
+})
