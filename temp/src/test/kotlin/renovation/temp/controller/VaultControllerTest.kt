7f9e537bc3d8531be7089ecce05359a0119b9825
koresmosto <koresmosto@gmail.com>
2023-11-25 17:49:22 +0200

    #0 Add e2eTest to temp vault

--- temp/src/test/kotlin/renovation/temp/controller/VaultControllerTest.kt
+++ temp/src/test/kotlin/renovation/temp/controller/VaultControllerTest.kt
@@ -1,3 +1,9 @@
+/*
+ * Created under not commercial project "Renovation"
+ *
+ * Copyright 2021-2023
+ */
+
 package renovation.temp.controller
 
 import io.restassured.module.kotlin.extensions.Given
@@ -8,34 +14,33 @@ import org.hamcrest.CoreMatchers
 import org.junit.jupiter.api.Tag
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.ActiveProfiles
+import renovation.common.util.Json
 
-@Tag("integrationTest")
-@TestPropertySource(
-    properties =
-    ["spring.autoconfigure.exclude=org.springframework.cloud.vault.config.VaultAutoConfiguration"]
-)
+@Tag("e2eTest")
+@ActiveProfiles("vault")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-internal class CoroutinesControllerTest(
-    @LocalServerPort val port: Int
+internal class VaultControllerTest(
+    @LocalServerPort private val port: Int,
 ) {
-    @Test
-    fun blocked() {
-        given()
-            .When {
-                get("/coroutines/default")
-            }.Then {
-                body(CoreMatchers.equalTo("Result is being printed in app console ..."))
-            }
-    }
 
     @Test
     fun unblocked() {
         given()
             .When {
-                get("/coroutines/blocking")
+                get("vault")
             }.Then {
-                body(CoreMatchers.equalTo("Result is being printed in app console (blocking) ..."))
+                body(CoreMatchers.equalTo(
+                    Json.rowJson(
+                        """
+                        [
+                            "Next Hello!",
+                            "Next there!!!"
+                        ]
+                        """.trimIndent()
+                    )
+                ))
+
             }
     }
 
