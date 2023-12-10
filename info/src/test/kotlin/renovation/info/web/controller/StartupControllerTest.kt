2b3cce22f902d631189b5794f3eccc8148870b6a
koresmosto <koresmosto@gmail.com>
2023-12-10 20:42:32 +0200

    #1 IntegrationTest marking

--- info/src/test/kotlin/renovation/info/web/controller/StartupControllerTest.kt
+++ info/src/test/kotlin/renovation/info/web/controller/StartupControllerTest.kt
@@ -11,9 +11,11 @@ import io.restassured.module.kotlin.extensions.When
 import kotlin.test.Test
 import org.apache.http.HttpStatus
 import org.hamcrest.CoreMatchers
+import org.junit.jupiter.api.Tag
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 
+@Tag("integrationTest")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 internal class StartupControllerTest(
     @LocalServerPort val port: Int
