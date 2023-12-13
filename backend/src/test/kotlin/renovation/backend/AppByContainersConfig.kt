446e58ccf669a93c96082e25db3c9fc700aaf1f3
koresmosto <koresmosto@gmail.com>
2023-12-13 03:24:09 +0200

    #1 backend module tests changes

--- backend/src/test/kotlin/renovation/backend/AppByContainersConfig.kt
+++ backend/src/test/kotlin/renovation/backend/AppByContainersConfig.kt
@@ -15,20 +15,4 @@ import org.springframework.test.context.ContextConfiguration
         RedisContainerConfig::class,
     ]
 )
-internal abstract class AppByContainersConfig {
-
-    // todo: change to ContainerConfig
-//    companion object {
-//
-//        @Container
-//        @ServiceConnection(name = "postgres")
-//        val postgresContainer: PostgreSQLContainer<Nothing> =
-//            PostgreSQLContainer<Nothing>("postgres:16.1-alpine")
-//
-//        @Container
-//        @ServiceConnection(name = "redis")
-//        val redisContainer: GenericContainer<Nothing> =
-//            GenericContainer<Nothing>(DockerImageName.parse("redis:7.2.3-alpine"))
-//                .withExposedPorts(6379)
-//    }
-}
+internal abstract class AppByContainersConfig
