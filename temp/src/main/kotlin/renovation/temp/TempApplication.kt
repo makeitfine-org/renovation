1b00bd03fdcf0883f24da6a1ecc4cf255f526a76
koresmosto <koresmosto@gmail.com>
2023-12-15 21:58:24 +0200

    #1 Impr. temp module

--- temp/src/main/kotlin/renovation/temp/TempApplication.kt
+++ temp/src/main/kotlin/renovation/temp/TempApplication.kt
@@ -8,14 +8,13 @@ package renovation.temp
 
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication(
-    exclude = [
-        SecurityAutoConfiguration::class,
-    ]
+//    exclude = [
+//        SecurityAutoConfiguration::class,
+//    ]
 )
 @EnableAsync
 class TempApplication
