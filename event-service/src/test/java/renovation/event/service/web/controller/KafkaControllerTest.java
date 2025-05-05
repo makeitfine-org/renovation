af27dced027c85aa2d5dd4f64ecb99a7c615d3fb
koresmosto <koresmosto@gmail.com>
2025-05-05 22:39:34 +0200

    #112 Refactoring of KafkaControllerTest

--- event-service/src/test/java/renovation/event/service/web/controller/KafkaControllerTest.java
+++ event-service/src/test/java/renovation/event/service/web/controller/KafkaControllerTest.java
@@ -7,51 +7,37 @@
 package renovation.event.service.web.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import io.restassured.RestAssured;
-import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ContextConfiguration;
 import renovation.event.service.service.consumer.WorkEventKafkaConsumer;
 import renovation.event.service.service.mapper.WorkEventRequestMapper;
 import renovation.event.service.web.Route;
 import renovation.event.service.web.controller.base.KafkaTestcontainersInit;
+import renovation.event.service.web.controller.base.RestTestInit;
 import renovation.event.service.web.dto.WorkEventRequest;
 
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-import static io.restassured.RestAssured.given;
 import static renovation.event.service.util.Helper.OBJECT_MAPPER;
-import static renovation.event.service.util.Helper.readFileContentFromProjectRoot;
 
 @Tag("componentTest")
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = KafkaTestcontainersInit.class)
-class KafkaControllerTest {
+class KafkaControllerTest extends RestTestInit {
 
-    @LocalServerPort
-    protected Integer port;
-    protected RequestSpecification request;
+    public KafkaControllerTest() {
+        super(Route.KAFKA);
+    }
 
     @Autowired
     private WorkEventKafkaConsumer consumer;
 
     @Autowired
     private WorkEventRequestMapper workEventRequestMapper;
-
-    @BeforeEach
-    void init() {
-        RestAssured.port = port;
-        this.request = given().header("Content-Type", "application/json")
-                .basePath(Route.KAFKA);
-    }
 
     @Test
     void when_publish_expect_Success() throws InterruptedException, JsonProcessingException {
@@ -88,9 +74,5 @@ class KafkaControllerTest {
 
     private void waitForSleepConsumption() throws InterruptedException {
         TimeUnit.MILLISECONDS.sleep(500); // todo: think of decreasing (future task)
-    }
-
-    protected String jsonFileContentFromSrcTestResources(String pathInSrcTestResources) {
-        return readFileContentFromProjectRoot("src/test/resources/json/" + pathInSrcTestResources);
     }
 }
