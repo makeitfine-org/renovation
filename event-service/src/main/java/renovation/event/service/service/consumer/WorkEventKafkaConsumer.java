a4e1bb0eebcfe926294d70db4cb9fc0fb50696fa
koresmosto <koresmosto@gmail.com>
2025-05-06 18:41:08 +0200

    #112 Kafka retries

--- event-service/src/main/java/renovation/event/service/service/consumer/WorkEventKafkaConsumer.java
+++ event-service/src/main/java/renovation/event/service/service/consumer/WorkEventKafkaConsumer.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import renovation.event.service.kafka.avro.record.work.WorkEvent;
 import renovation.event.service.kafka.avro.record.work.WorkEventKey;
@@ -19,9 +21,20 @@ public class WorkEventKafkaConsumer {
     public static final int LAST_ELEMENT_SAVING_QUEUE_CAPACITY = 1;
     public static final int DEFAULT_LAST_ELEMENT_READ_WAIT_TIME_MILLIS = 5000;
 
+    public static final int RETRY_MAX_ATTEMPTS = 3;
+    public static final int RETRY_DELAY = 1000;
+    public static final int RETRY_MAX_DELAY = 3000;
+
     private BlockingQueue<ImmutablePair<WorkEventKey, WorkEvent>> q =
             new LinkedBlockingQueue<>(LAST_ELEMENT_SAVING_QUEUE_CAPACITY);
 
+    // todo: consider "Scaling Event-Driven Microservices"
+    // https://medium.com/@bubu.tripathy/event-driven-architecture-adb658a1dc9c
+    @Retryable(
+            retryFor = Exception.class,
+            maxAttempts = RETRY_MAX_ATTEMPTS,
+            backoff = @Backoff(delay = RETRY_DELAY, maxDelay = RETRY_MAX_DELAY)
+    )
     @KafkaListener(
             topics = "${spring.kafka.topic.name}",
             groupId = "${spring.kafka.consumer.group-id}"
@@ -31,6 +44,9 @@ public class WorkEventKafkaConsumer {
 
         q.clear();
         q.offer(ImmutablePair.of(consumerRecord.key(), consumerRecord.value()));
+
+        // todo: implement dead-letter queue for messages with a failed to process:
+        // https://medium.com/@bubu.tripathy/event-driven-architecture-adb658a1dc9c
     }
 
     /**
