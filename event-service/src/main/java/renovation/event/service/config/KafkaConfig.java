721620daa0d403486209c7521f5720cdf5a66143
koresmosto <koresmosto@gmail.com>
2025-05-07 21:10:32 +0200

    #112 Kafka streams intro refactoring

--- event-service/src/main/java/renovation/event/service/config/KafkaConfig.java
+++ event-service/src/main/java/renovation/event/service/config/KafkaConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -18,6 +19,7 @@ import java.util.Map;
 
 @Configuration
 @EnableKafka
+@EnableKafkaStreams
 public class KafkaConfig {
 
     @Bean
