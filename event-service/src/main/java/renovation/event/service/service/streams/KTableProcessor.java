04e5f8f6a8605a01cdd333a77a153dcca58f2759
koresmosto <koresmosto@gmail.com>
2025-05-10 00:29:01 +0200

    #112 Kafka streams (draft): aggregate

--- event-service/src/main/java/renovation/event/service/service/streams/KTableProcessor.java
+++ event-service/src/main/java/renovation/event/service/service/streams/KTableProcessor.java
@@ -9,12 +9,15 @@ package renovation.event.service.service.streams;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KGroupedStream;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
@@ -42,27 +45,32 @@ public class KTableProcessor {
 
     public void process(KStream<WorkEventKey, WorkEvent> stream) {
         //Create a new KeyValue Store
-//        KeyValueBytesStoreSupplier worksByIdPriceStore = Stores.persistentKeyValueStore(storeName);
-
-//        KGroupedStream<WorkEventKey, WorkEvent> worksById = stream.groupByKey();
-//
-//        KTable<WorkEventKey, Long> workByIdCount = worksById.count();
+        //KeyValueBytesStoreSupplier worksByIdPriceStore = Stores.persistentKeyValueStore(storeName);
 
         KGroupedStream<String, Double> worksById = stream
                 .map((key, work) -> new KeyValue<>(String.valueOf(work.getId()), work.getPrice()))
                 .groupByKey(Grouped.with(Serdes.String(), Serdes.Double()));
 
-        KTable<String, Long> workByIdCount = worksById.count();
-        workByIdCount.toStream().foreach((k, v) -> {
+        KTable<String, Long> workByIdCount = worksById.count(
+                Materialized.<String, Long, KeyValueStore<org.apache.kafka.common.utils.Bytes, byte[]>>as("count-store")
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.Long())
+                        .withLoggingDisabled()
+        );
+        workByIdCount.toStream().peek((k, v) -> {
             log.info("id -> count : {} -> {} ", k, v);
-        });
+        }).to("count", Produced.with(Serdes.String(), Serdes.Long()));
 
         KTable<String, Double> workByIdTotalPrice = worksById.reduce(
-                Double::sum
-        );
-        workByIdTotalPrice.toStream().foreach((k, v) -> {
+                Double::sum,
+                Materialized.<String, Double, KeyValueStore<Bytes, byte[]>>as("total-price-store")
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.Double())
+                        .withLoggingDisabled()
+                );
+        workByIdTotalPrice.toStream().peek((k, v) -> {
             log.info("id -> total price : {} -> {} ", k, v);
-        });
+        }).to("totalPrice", Produced.with(Serdes.String(), Serdes.Double()));
 
         Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url", schemaRegistry);
         SpecificAvroSerde<WorkAggregateEvent> aggregateSerde = new SpecificAvroSerde<>();
@@ -77,59 +85,14 @@ public class KTableProcessor {
                                 aggregate.getPriceSum() + newPrice
                         ),
                 // Materialized with Serdes
-                Materialized.with(Serdes.String(), aggregateSerde)
+                Materialized.<String, WorkAggregateEvent, KeyValueStore<org.apache.kafka.common.utils.Bytes, byte[]>>as("avro-agg-store")
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(aggregateSerde)
+                        .withLoggingDisabled()
         );
 
-
-//        KGroupedStream<String, Double> worksById = stream
-//                .map((key, work) -> new KeyValue(work.getId().toString(), work.getPrice()))
-//                .groupByKey();
-
-//        log.info(">>> {}", worksById.count());
-
-//        final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url", schemaRegistry);
-//        final SpecificAvroSerde<WorkAggregateEvent> valueSpecificAvroSerde = new SpecificAvroSerde<>();
-//        valueSpecificAvroSerde.configure(serdeConfig, false);
-
-
-//        KTable<String, WorkAggregateEvent> workAggregate = worksById.aggregate(
-//                () -> WorkAggregateEvent.newBuilder().setCount(0).setPriceSum(0.0).build(),
-//                (key, value, aggregate) -> WorkAggregateEvent.newBuilder()
-//                        .setCount(aggregate.getCount() + 1)
-//                        .setPriceSum(aggregate.getPriceSum() + value)
-//                        .build(),
-//                Materialized.with(Serdes.String(), valueSpecificAvroSerde)
-//        );
-
-//        KTable<String, WorkAggregateEvent> workAggregate = worksById.aggregate(
-//                () -> WorkAggregateEvent.newBuilder().setCount(0).setPriceSum(0.0).build(),
-//                (key, value, aggregate) -> {
-//                    if (aggregate == null) {
-//                        return WorkAggregateEvent.newBuilder()
-//                                .setCount(1)
-//                                .setPriceSum(value)
-//                                .build();
-//                    } else {
-//                        return WorkAggregateEvent.newBuilder()
-//                                .setCount(aggregate.getCount() + 1)
-//                                .setPriceSum(aggregate.getPriceSum() + value)
-//                                .build();
-//                    }
-//                },
-//                Materialized.with(Serdes.String(), valueSpecificAvroSerde)
-//        );
-
-//        final KTable<String, Double> workTotal =
-//                workAggregate.mapValues(
-//                        value -> value.getPriceSum(),
-//                        Materialized.as(worksByIdPriceStore)
-//                );
-//
-//        final KTable<String, Double> workTotal = workAggregate.mapValues(
-//                WorkAggregateEvent::getPriceSum,
-//                Materialized.<String, Double>as(worksByIdPriceStore)
-//                        .withKeySerde(Serdes.String())
-//                        .withValueSerde(Serdes.Double())
-//        );
+        workByIdAggregate.toStream().peek((k, v) ->
+                log.info("AGG: id={} => count={}, sum={}", k, v.getCount(), v.getPriceSum())
+        ).to("avroTotalPrice", Produced.with(Serdes.String(), aggregateSerde));
     }
 }
