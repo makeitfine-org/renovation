0057ca46c69db8f36f8d3328e833e8e00a35e8b7
stingion <stingion@gmail.com>
2025-07-16 11:17:44 +0200

    #116 Remove issue_116 github build changes

--- temp/src/test/java/renovation/temp/java/Java8Test.java
+++ temp/src/test/java/renovation/temp/java/Java8Test.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 interface ITemp {
 
-    default String method() {
+    default String method() throws IllegalArgumentException {
         return "default method";
     }
 }
