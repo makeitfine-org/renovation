15431ccb9061b40b651ebe9c4378d66e3a75bfdf
stingion <stingion@gmail.com>
2025-08-12 12:44:27 +0200

    #69 Smoke test of renovation cluster working

--- a-deploy/test/test.sh
+++ a-deploy/test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x // verbose commands
+#set -x // verbose commands
 
 #MINIKUBE_PATH="`dirname \"$0\"`"
 #K8S_PATH="${MINIKUBE_PATH}/../../k8s"
@@ -33,9 +33,10 @@ export pg_schema='backend';
 
 # test postgres
 if nc -zv "$CLUSTER_IP" "$POSTGRES_CLUSTER_PORT" 2>&1 | grep -q 'succeeded'; then
-  echo "✅ Connection to $CLUSTER_IP:$POSTGRES_CLUSTER_PORT succeeded"
+  echo "✅ Connection to PostgreSQL ($CLUSTER_IP:$POSTGRES_CLUSTER_PORT) succeeded"
 else
-  echo "❌ Connection to $CLUSTER_IP:$POSTGRES_CLUSTER_PORT failed"
+  echo "❌ Connection to PostgreSQL ($CLUSTER_IP:$POSTGRES_CLUSTER_PORT) failed"
+  exit;
 fi
 
 if PGPASSWORD="$pg_password" psql -h "$CLUSTER_IP" -p "$POSTGRES_CLUSTER_PORT" -U "$pg_username" -d "$pg_db" -c '\q' 2>/dev/null; then
@@ -52,12 +53,21 @@ else
   exit;
 fi
 
-echo 'hello'
-
-exit;
 # test redis
+if nc -zv "$CLUSTER_IP" "$REDIS_CLUSTER_PORT" 2>&1 | grep -q 'succeeded'; then
+  echo "✅ Connection to Redis ($CLUSTER_IP:$REDIS_CLUSTER_PORT) succeeded"
+else
+  echo "❌ Connection to Redis ($CLUSTER_IP:$REDIS_CLUSTER_PORT) failed"
+  exit;
+fi
 
 # test mongo
+if nc -zv "$CLUSTER_IP" "$MONGO_CLUSTER_PORT" 2>&1 | grep -q 'succeeded'; then
+  echo "✅ Connection to Mongo ($CLUSTER_IP:$MONGO_CLUSTER_PORT) succeeded"
+else
+  echo "❌ Connection to Mongo ($CLUSTER_IP:$MONGO_CLUSTER_PORT) failed"
+  exit;
+fi
 
 # test apps:
 newman run renovation-minikube.postman_collection.json -e renovation-minikube-env.postman_environment.json
